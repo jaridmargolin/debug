bd79b081b20c13816410217cb368ef041884eedb
Jarid Margolin <jaridmargolin@gmail.com>
2019-12-21 23:52:18 -0800

    Update build process to utilize core-js

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,9 @@
  * dependencies
  * -------------------------------------------------------------------------- */
 
+// 3rd party
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import babel from 'rollup-plugin-babel'
 
@@ -11,7 +14,22 @@ import babel from 'rollup-plugin-babel'
  * rollup config
  * -------------------------------------------------------------------------- */
 
+const externals = [
+  'core-js/modules/es.array.concat',
+  'core-js/modules/es.array.map',
+  'core-js/modules/es.array.some',
+  'core-js/modules/es.regexp.constructor',
+  'core-js/modules/es.regexp.exec',
+  'core-js/modules/es.regexp.to-string',
+  'core-js/modules/es.string.replace',
+  'core-js/modules/es.string.split'
+]
+
 const sharedPlugins = [
+  resolve({
+    extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json']
+  }),
+  commonjs(),
   babel({
     extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx'],
     exclude: 'node_modules/**'
@@ -21,16 +39,7 @@ const sharedPlugins = [
 export default [
   {
     input: 'src/debug.ts',
-    plugins: [...sharedPlugins],
-    output: {
-      file: 'dist/debug.js',
-      format: 'umd',
-      name: 'debugProp',
-      exports: 'named'
-    }
-  },
-  {
-    input: 'src/debug.ts',
+    external: externals,
     plugins: [...sharedPlugins],
     output: {
       file: 'dist/common/debug.js',
@@ -40,10 +49,21 @@ export default [
   },
   {
     input: 'src/debug.ts',
+    external: externals,
     plugins: [...sharedPlugins],
     output: {
       file: 'dist/es/debug.js',
       format: 'es'
+    }
+  },
+  {
+    input: 'src/debug.ts',
+    plugins: [...sharedPlugins],
+    output: {
+      file: 'dist/debug.js',
+      format: 'umd',
+      name: 'debugProp',
+      exports: 'named'
     }
   },
   {
